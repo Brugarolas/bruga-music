ad155bca1d06399c5c02594b35e8a8dfce46b7b2
Andrés Brugarolas <brugarolas.92@gmail.com>
2019-05-19 17:35:50 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -111,7 +111,7 @@ module.exports = {
       meta: {
         viewport: 'width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no'
       },
-      inject: true
+      inject: 'body'
     }),
     new HtmlWebpackIncludeAssetsPlugin({
       assets: ['styles/fonts.css'],
