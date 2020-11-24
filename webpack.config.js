ccfa5978df5baf5f5d2a7c154646b7f721ba7369
Andrés Brugarolas <brugarolas.92@gmail.com>
2020-11-24 01:44:51 +0100

    iPhone X 'notch' CSS fixes

--- webpack.config.js
+++ webpack.config.js
@@ -118,7 +118,7 @@ module.exports = {
       filename: './index.html',
       favicon: './src/assets/logo.png',
       meta: {
-        viewport: 'width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no'
+        viewport: 'width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no, viewport-fit=cover'
       },
       inject: 'body'
     })
